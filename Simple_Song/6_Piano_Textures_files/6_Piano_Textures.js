6fc4470695c7d4bd0ba83bb0705f5061c9dc32a1
bj-brooks <octatonic@gmail.com>
2025-03-17 16:00:00 -0500

    Add files via upload

---
+++ Simple_Song/6_Piano_Textures_files/6_Piano_Textures.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140403
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('6_Piano_Textures_files/6_Piano_TexturesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Simple_Song/Writing_Steps_files/Writing_Steps.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140403
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Writing_Steps_files/Writing_StepsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
